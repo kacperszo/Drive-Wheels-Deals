7ea179437e8755d09c34f6dde0b3e94477fd2404
Kacper Szot <kacperszot1@gmail.com>
2024-06-18 22:36:27 +0200

    add Creation Timestamp to the order model

--- src/main/java/pl/drivewheelsdeals/app/model/Order.java
+++ src/main/java/pl/drivewheelsdeals/app/model/Order.java
@@ -2,6 +2,7 @@ package pl.drivewheelsdeals.app.model;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.math.BigDecimal;
 import java.sql.Timestamp;
@@ -20,6 +21,7 @@ public class Order {
     private Customer customer;
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<OrderItem> items;
+    @CreationTimestamp
     private Timestamp orderDate;
     private BigDecimal totalDiscount;
 
